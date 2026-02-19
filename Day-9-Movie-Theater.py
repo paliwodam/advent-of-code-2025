7890ca07a1bf256d47ddba9d86f1d565c19f4991
unknown <martynapaliwoda.2001@gmail.com>
2026-02-19 10:04:00 +0100

    [Day 9] Movie Theater - 2 stars ⭐

--- Day-9-Movie-Theater.py
+++ Day-9-Movie-Theater.py
@@ -1,13 +1,31 @@
+import shapely
+from shapely import Polygon
+
 input_file = "inputs/day9.txt"
 with open(input_file) as file:
     coords = [line.split(",") for line in file.readlines()]
     coords = [(int(x[0]), int(x[1])) for x in coords]
     
-largest_area = 0
 
-for i, j in coords:
-    for k, l in coords:
-        area = (abs(k-i)+1) * (abs(l-j)+1)
-        largest_area = max(area, largest_area)
+def part1():
+    largest_area = 0
+    for i, j in coords:
+        for k, l in coords:
+            area = (abs(k-i)+1) * (abs(l-j)+1)
+            largest_area = max(area, largest_area)
+    return largest_area
 
-print(largest_area)
+def part2():
+    largest_area = 0
+    polygon = Polygon(coords)
+    for i, j in coords:
+        for k, l in coords:
+            rect = shapely.box(i, j, k, l)
+            if not shapely.within(rect, polygon):
+                continue
+            area = (abs(k-i)+1) * (abs(l-j)+1)
+            largest_area = max(area, largest_area)
+    return largest_area
+
+
+print(part2())
