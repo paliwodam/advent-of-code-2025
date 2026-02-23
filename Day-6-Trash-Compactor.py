35ce54cfc76ef64ae5d1b68bc15ea13236316b06
unknown <martynapaliwoda.2001@gmail.com>
2026-02-23 10:04:34 +0100

    [Day 6] Trash Compactor - 2 stars ⭐

--- Day-6-Trash-Compactor.py
+++ Day-6-Trash-Compactor.py
@@ -5,16 +5,41 @@ with open(input_file) as file:
     operators = operators.split()
     numbers = [[int(x) for x in line.split()] for line in numbers]
 
-rows, columns = len(numbers), len(operators)
+def part1():
+    rows, columns = len(numbers), len(operators)
+    total = 0
+    for i in range(columns):
+        result = 1 if operators[i] == "*" else 0
+        for j in range(rows):
+            if operators[i] == "*": 
+                result *= numbers[j][i]
+            else:
+                result += numbers[j][i]
+        total += result
+    return total
 
-total = 0
-for i in range(columns):
-    result = 1 if operators[i] == "*" else 0
-    for j in range(rows):
-        if operators[i] == "*": 
-            result *= numbers[j][i]
-        else:
-            result += numbers[j][i]
-    total += result
+def part2():
+    n, m = len(homework), len(homework[0])
+    
+    column_start = []
+    for idx, c in enumerate(homework[-1]):
+        if c == "*" or c == "+": 
+            column_start.append(idx)
 
-print(total)
+    total = 0
+    for idx, start in enumerate(column_start):
+        operator = operators[idx]
+        end = m + 1 if idx == len(column_start)-1 else column_start[idx+1]
+        result = 1 if operator == "*" else 0
+        for j in range(start, end-1):
+            number = 0
+            for i in range(n-1):
+                if homework[i][j] != ' ':
+                    number *= 10 
+                    number += int(homework[i][j])
+            if operator == "*": 
+                result *= number
+            else:
+                result += number
+        total += result
+    return total
