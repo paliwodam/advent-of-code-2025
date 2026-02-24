515941236ddf16123de9019cc5cfcea341353f48
unknown <martynapaliwoda.2001@gmail.com>
2026-02-24 23:57:02 +0100

    [Day 3] Lobby - 2 stars ⭐

--- Day-3-Lobby.py
+++ Day-3-Lobby.py
@@ -2,12 +2,24 @@ input_file = "inputs/day3.txt"
 with open(input_file) as file:
     joltage_ratings = [line.strip() for line in file.readlines()]
 
-result = 0
-for joltage_rating in joltage_ratings:
-    n = len(joltage_rating)
-    max_number = max(joltage_rating[:n-1])
-    max_index = joltage_rating.index(max_number)
-    second_max_number = max(joltage_rating[max_index+1:])
-    result += int(max_number+second_max_number)
+def batteries(numeber):
+    result = 0
+    for joltage_rating in joltage_ratings:
+        offset, start, end = 0, 0, len(joltage_rating)
+        joltage = ''
+        for i in range(numeber-1, -1, -1):
+            max_number = max(joltage_rating[start+offset:end-i])
+            joltage += max_number
+            offset += joltage_rating[start+offset:].index(max_number) + 1
+        result += int(joltage)
+    return result
 
-print(result)
+def part1():
+    return batteries(2)
+
+def part2():
+    return batteries(12)
+
+            
+
+    
