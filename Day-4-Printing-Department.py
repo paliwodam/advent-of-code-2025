1076314f051310af7c3b23814226d9731e7e73d7
unknown <martynapaliwoda.2001@gmail.com>
2026-02-24 23:56:44 +0100

    [Day 4] Printing Department - 2 stars ⭐

--- Day-4-Printing-Department.py
+++ Day-4-Printing-Department.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 input_file = "inputs/day4.txt"
 with open(input_file) as file:
     diagram = [list(line.strip()) for line in file.readlines()]
@@ -7,17 +9,37 @@ n, m = len(diagram), len(diagram[0])
 counts = [[0 for _ in range(m)] for _ in range(n)]
 adjacents = [(i, j) for i in [-1, 0, 1] for j in [-1, 0, 1]]
 
-result = 0
-
-for i in range(n):
-    for j in range(m):
-        if diagram[i][j] != "@":
-            continue
-        rolls = -1
-        for x, y in adjacents:
-            if not (0 <= i+x < n) or not (0 <= j+y < m):
+def accessible_rolls(diagram):
+    count = 0
+    rolls = []
+    for i in range(n):
+        for j in range(m):
+            if diagram[i][j] != "@":
                 continue
-            if diagram[i+x][j+y] == "@": rolls += 1
-        if rolls < 4: result += 1
+            adj_rolls = -1
+            for x, y in adjacents:
+                if not (0 <= i+x < n) or not (0 <= j+y < m):
+                    continue
+                if diagram[i+x][j+y] == "@": adj_rolls += 1
+            if adj_rolls < 4: 
+                rolls.append((i, j))
+                count += 1
+    return count, rolls
 
-print(result)
+def part1():
+    return accessible_rolls(diagram)[0]
+
+def part2():
+    result = 0
+    updated_diagram = deepcopy(diagram)
+
+    while True:
+        count, rolls_to_remove = accessible_rolls(updated_diagram)
+        if count == 0: break
+        result += count
+        for i, j in rolls_to_remove:
+            updated_diagram[i][j] = "."
+    return result
+
+print(part1())
+print(part2())
