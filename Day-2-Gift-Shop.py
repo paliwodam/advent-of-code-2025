baab858251b088b5d96858ae2c14a7109ba6b006
unknown <martynapaliwoda.2001@gmail.com>
2026-02-25 08:19:45 +0100

    [Day 2] Gift Shop - 2 stars ⭐

--- Day-2-Gift-Shop.py
+++ Day-2-Gift-Shop.py
@@ -2,14 +2,22 @@ input_file = "inputs/day2.txt"
 with open(input_file) as file:
     id_ranges = [r.split('-') for r in file.read().split(',')]
 
-result = []
-for id_range in id_ranges:
-    first_id, last_id = id_range
-    for id in range (int(first_id), int(last_id)+1):
-        id_s = str(id)
-        n = len(id_s) // 2
-        if id_s[:n] == id_s[n:]:
-            result.append(id)
+def invalid_ids(part):
+    result = []
+    for id_range in id_ranges:
+        first_id, last_id = id_range
+        for id in range (int(first_id), int(last_id)+1):
+            id_s = str(id)
+            n = len(id_s)
+            for i in range(2,n+1):
+                if n % i == 0 and id_s[:(n//i)] * i == id_s:
+                    result.append(id)
+                    break
+                if part == 1: break
+    return sum(result)
 
-print(sum(result))
+def part1():
+    return invalid_ids(1)
 
+def part2():
+    return invalid_ids(2)
